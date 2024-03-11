a1c66cce0cf1f2a965c18b68111e382ee4168ad1
pro-masud <promasudbd@gmail.com>
2024-03-11 10:31:43 +0600

    dynamic menu router here now

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./header.scss";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from "../../assets/logo.png";
 
 const Header = () => {
+  const location = useLocation();
   return (
     <>
       <header>
@@ -15,12 +16,12 @@ const Header = () => {
           </div>
           <div className="header-right">
             <ul className='menu'>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/admin">Admin</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/team">Team</Link></li>
-              <li><Link to="/contact">Contact Us</Link></li>
+              <li><Link className={location.pathname === "/" ? "active" : " "} to="/">Home</Link></li>
+              <li><Link className={location.pathname === "/admin" ? "active" : " "} to="/admin">Admin</Link></li>
+              <li><Link className={location.pathname === "/about" ? "active" : " "} to="/about">About</Link></li>
+              <li><Link className={location.pathname === "/blog" ? "active" : " "} to="/blog">Blog</Link></li>
+              <li><Link className={location.pathname === "/team" ? "active" : " "} to="/team">Team</Link></li>
+              <li><Link className={location.pathname === "/contact" ? "active" : " "} to="/contact">Contact Us</Link></li>
             </ul>
           </div>
         </div>

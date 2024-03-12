41afcf1aacc8cbadc827170521a46aa9ba61a829
pro-masud <promasudbd@gmail.com>
2024-03-13 00:28:16 +0600

    react form management system design complete by masud

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import HelmetHas from '../../component/helmetHas/HelmetHas';
+import "./contact.scss";
 
 const Contact = () => {
   return (
     <>
       <HelmetHas title={"Contact"}/>
-      <h2>contact us page</h2>
+      <div className="container container-contact-form">
+        <h2>contact us page</h2>
+        <div className="contact-form">
+          <div className="react-form">
+            <form action="">
+              <input type="text" placeholder='User Name' />
+              <input type="text" placeholder='Email' />
+              <input type="text" placeholder='Password' />
+              <input type="text" placeholder='Message'/>
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

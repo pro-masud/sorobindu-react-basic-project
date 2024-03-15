f35400de24f0b17d602950cfb13f47466544f05a
pro-masud <promasudbd@gmail.com>
2024-03-15 11:50:55 +0600

    react basic form management system

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HelmetHas from '../../component/helmetHas/HelmetHas';
 import "./contact.scss";
 
 const Contact = () => {
+  // get value for react js here
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <>
       <HelmetHas title={"Contact"}/>
       <div className="container container-contact-form">
         <h2>contact us page</h2>
+        <hr />
+        <p>{name}</p>
         <div className="contact-form">
           <div className="react-form">
             <form action="">
-              <input type="text" placeholder='User Name' />
-              <input type="text" placeholder='Email' />
-              <input type="text" placeholder='Password' />
-              <input type="text" placeholder='Message'/>
+              <input type="text" placeholder='User Name' value={name} onChange={(e) => setName(e.target.value)}/>
+              <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+              <input type="text" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+              <input type="text" placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)} />
               <button type='submit'>Submit</button>
             </form>
           </div>

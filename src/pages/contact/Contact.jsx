c4c0c6cce73764e8a2a6026639574422f8a9e086
pro-masud <promasudbd@gmail.com>
2024-03-15 12:11:22 +0600

    react contact form manage for starnder way solution

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
-import HelmetHas from '../../component/helmetHas/HelmetHas';
+import React, { useState } from "react";
+import HelmetHas from "../../component/helmetHas/HelmetHas";
 import "./contact.scss";
 
 const Contact = () => {
   // get value for react js here
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+    message: "",
+  });
+
   return (
     <>
-      <HelmetHas title={"Contact"}/>
+      <HelmetHas title={"Contact"} />
       <div className="container container-contact-form">
         <h2>contact us page</h2>
         <hr />
@@ -18,17 +21,28 @@ const Contact = () => {
         <div className="contact-form">
           <div className="react-form">
             <form action="">
-              <input type="text" placeholder='User Name' value={name} onChange={(e) => setName(e.target.value)}/>
-              <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-              <input type="text" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-              <input type="text" placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)} />
-              <button type='submit'>Submit</button>
+              <input
+                type="text"
+                placeholder="User Name"
+                value={input.name}
+                onChange={(e) =>
+                  setInput((preValue) => ({...preValue, name: e.target.value }))
+                }
+              />
+              <input type="text" placeholder="Email" value={input.email} onChange={(e) => setInput((preValue) => ({...preValue, email: e.target.value}))}/>
+              <input
+                type="text"
+                placeholder="Password"
+                value={input.password} onChange={(e) => setInput((preValue) => ({...preValue, password: e.target.value}))}
+              />
+              <input type="text" placeholder="Message" value={input.message} onChange={(e) => setInput((preValue) => ({...preValue, message: e.target.value}))} />
+              <button type="submit">Submit</button>
             </form>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Contact;

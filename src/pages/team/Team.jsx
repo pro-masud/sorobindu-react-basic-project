acba3b7df74d1e6f5f8bfff91bb0c1862f6f5cf4
pro-masud <promasudbd@gmail.com>
2024-03-15 16:23:54 +0600

    react js useEffect hook working successfully working

--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import HelmetHas from '../../component/helmetHas/HelmetHas'
+import React, { useEffect, useState } from 'react'
+import HelmetHas from '../../component/helmetHas/HelmetHas';
+import "./team.scss";
 
 const Team = () => {
+
+  const [age, setAge]  = useState("");
+  const [couter, setCouter]  = useState(0);
+
+  useEffect(() => {
+    console.log("hello world");
+  }, [age]);
+
+  useEffect(() => {
+    console.log("counter value change");
+  }, [couter])
+
+  
   return (
     <>
       <HelmetHas title={"Team"}/>
-      <h2>Team Member Page</h2>
+      <div className="container team-container">
+        <div className="couter">
+          <input type="text" placeholder='Inter Your Age' value={age} onChange={(e) => setAge(e.target.value)} />
+          <hr />
+          <h2>{couter}</h2>
+          <hr />
+          <button onClick={() => setCouter((preValue) => preValue - 1)} >--</button>
+          <button onClick={() => setCouter((preValue) => preValue + 1)}>++</button>
+        </div>
+      </div>
     </>
   )
 }
 
-export default Team
+export default Team;

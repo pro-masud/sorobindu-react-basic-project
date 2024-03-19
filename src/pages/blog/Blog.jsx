8d70e33b22dcd61dc3d2e44c988dfec026f3eca9
pro-masud <promasudbd@gmail.com>
2024-03-20 01:00:45 +0600

    react useContext data passing successfully working

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import HelmetHas from '../../component/helmetHas/HelmetHas'
+import CounterContext from '../../context/CounterContext';
 
 const Blog = () => {
+  const { foods } = useContext(CounterContext);
+  const {count, setCount } = useContext(CounterContext); // Correct usage
   return (
     <>
       <HelmetHas title={"Blog"}/>
-      <h2>blog page</h2>
+      <h2>blog page {count}</h2>
     </>
   )
 }
--- src/providers/counterProvider.jsx
+++ src/providers/counterProvider.jsx
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 import CounterContext from '../context/CounterContext';
 
+const foods = ['alo', 'potol', 'lao'];
+
 export const CounterProvider = ({ children }) => {
 
     const [count, setCount] = useState(0);
 
     return (
-        <CounterContext.Provider value={{ count, setCount }}>
+        <CounterContext.Provider value={{ count, setCount, foods }}>
             {children}
         </CounterContext.Provider>
     );
